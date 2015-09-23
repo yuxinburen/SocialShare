c6d366d2ff5ab9805be665dcc317e7180c41d4f7
chygithub <chy388288>
2015-09-23 17:21:33 +0800

    add the socialshare project to host

---
+++ app/src/main/java/com/example/socialshare/socialshare/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.socialshare.socialshare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by davie on 2015/9/23.
+ * Email : 861574834@qq.com
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
